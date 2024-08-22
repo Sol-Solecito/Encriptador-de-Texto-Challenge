d2ddbdea2caa2782aa26d989335370d3ab374f92
Sol-Solecito <soldreads8@gmail.com>
2024-08-21 22:48:32 -0500

    primer commit
    se publica archivos: HTML, Css, imagenes y Js

---
+++ README.md
@@ -0,0 +1,8 @@
+# Encriptador de Texto 
+
+¡Bienvenidos a mi proyecto de encriptador de texto!
+
+He desarrollado una aplicación web que permite encriptar y desencriptar textos, de manera que puedas intercambiar mensajes secretos con otras personas que conozcan el secreto de la encriptación utilizada.
+
+## Estado del proyecto:
+Proyecto Concluido.
---
+++ index.js
@@ -0,0 +1,89 @@
+function encriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muneco = document.getElementById("muneco");
+    let evaluar = document.getElementById("evaluar");
+    let copiar = document.getElementById("btn-copiar");
+    let gif = document.getElementById("gif");
+    
+    let textoCifrado = texto
+        .replace(/e/gi, "enter")
+        .replace(/i/gi, "imes")
+        .replace(/a/gi, "ai")
+        .replace(/o/gi, "ober")
+        .replace(/u/gi, "ufat")
+
+    if (texto.length |=0) {
+        document.getElementById("texto").value = textoCifrado.toLowerCase();
+        tituloMensaje.textContent = "Texto Encriptado con Éxito"
+        parrafo.textContent= "";
+        evaluar.textContent = textoCifrado;
+        muneco= muneco. remove();
+        copiar = copiar.style.display="block";
+        gif = gif.style.display="block";
+
+    }else{
+        muneco.src = "/Muñeco@2x.png";
+        tituloMensaje.textContent = "Ningun mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+        Swal.fire({
+            title: "oops!",
+            text: "Debes ingresar algún texto.",
+            imageUrl: "/alerta.gif",
+            imageWidth: 100,
+            imageHeight: 100,
+            imageAlt: "Custom image"
+        });
+    }
+}
+function desencriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muneco = document.getElementById("muneco");
+    let evaluar = document.getElementById("evaluar");
+    let copiar = document.getElementById("btn-copiar");
+    let gif = document.getElementById("gif");
+
+    let textoCifrado = texto .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ai/gi, "a")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u")
+
+    if (texto.length |=0) {
+        document.getElementById("texto").value = textoCifrado.toLowerCase();
+        tituloMensaje.textContent = "Texto Desencriptado con Éxito"
+        parrafo.textContent="";
+        evaluar.textContent = textoCifrado;
+        muneco= muneco.remove();
+        copiar = copiar.style.display="block";
+        gif = gif.style.display="block";
+        
+    } else{
+        muneco.src = "/Muñeco@2x.png";
+        tituloMensaje.textContent = "Ningun mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+        Swal.fire({
+            title: "oops!",
+            text: "Debes ingresar algún texto.",
+            imageUrl: "/alerta.gif",
+            imageWidth: 100,
+            imageHeight: 100,
+            imageAlt: "Custom image"
+        });
+    }
+} 
+function copiado(){
+    let evaluar = document.getElementById("evaluar");
+
+    navigator.clipboard.writeText(evaluar.value);
+    Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "Texto copiado al Portapapeles",
+        showConfirmButton: false,
+        timer: 1500
+    });
+} 
