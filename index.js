df461ca45c7659c6da31a7266ab0e1de6f2a4b40
Sol-Solecito <soldreads8@gmail.com>
2024-08-22 11:06:37 -0500

    actualiza fondo

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function encriptar() {
         gif = gif.style.display="block";
 
     }else{
-        muneco.src = "Muñeco@2x.png";
+        muneco.src = "imagenes/";
         tituloMensaje.textContent = "Ningun mensaje fue encontrado";
         parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
         Swal.fire({
